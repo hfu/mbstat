38cf665e8e3a667a04ab02d3fb6277ed40111bdf
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-05-04 15:16:14 +0000

    ready for multiple .mbtiles files

--- mbstat.js
+++ mbstat.js
@@ -11,32 +11,36 @@ const _q = v => {
 }
 
 if (process.argv.length != 3) {
-  console.log('Usage: node mbstat.js some.mbtiles')
+  console.log('Usage: node mbstat.js {mbtiles...}')
   process.exit()
 }
 
-const db = new sqlite3.Database(process.argv[2], sqlite3.OPEN_READONLY)
-db.each('SELECT * FROM tiles', (err, row) => {
-  if (err) return
-  const z = row.zoom_level
-  const x = row.tile_column
-  const y = (1 << z) - row.tile_row - 1
-  const size = row.tile_data.length
-  const q = _q(size)
-  // if (q <= 16) return
-  const tile = new VectorTile(new Protobuf(zlib.gunzipSync(row.tile_data)))
-  let r = {}
-  let r2 = {}
-  for (const l in tile.layers) {
-    r[l] = tile.layers[l].length
-  }
-  for (const l of ['polygon']) {
-    for (let i = 0; i < tile.layers[l].length; i++) {
-      const f = tile.layers[l].feature(i)
-      r2[f.properties.layer] = (r2[f.properties.layer] === undefined) ? 0 : (r2[f.properties.layer] + 1)
-      //console.log(JSON.stringify(f.toGeoJSON(x, y, z), null, 2))
+for (let i = 2; i < process.argv.length; i++) {
+  const db = new sqlite3.Database(process.argv[i], sqlite3.OPEN_READONLY)
+  db.each('SELECT * FROM tiles', (err, row) => {
+    if (err) return
+    const z = row.zoom_level
+    const x = row.tile_column
+    const y = (1 << z) - row.tile_row - 1
+    const size = row.tile_data.length
+    const q = _q(size)
+    if (q <= 16) return
+    const tile = new VectorTile(new Protobuf(zlib.gunzipSync(row.tile_data)))
+    let r = {}
+    let r2 = {}
+    for (const l in tile.layers) {
+      r[l] = tile.layers[l].length
     }
-  }
-  console.log(`${z}/${x}/${y}(${q}) ${JSON.stringify(r)} ${JSON.stringify(r2)}`)
-})
-db.close()
+    for (const l of ['polygon']) {
+      for (let i = 0; i < tile.layers[l].length; i++) {
+        const f = tile.layers[l].feature(i)
+        r2[f.properties.layer] = (r2[f.properties.layer] === undefined) ? 
+          1 : (r2[f.properties.layer] + 1)
+        //console.log(JSON.stringify(f.toGeoJSON(x, y, z), null, 2))
+      }
+    }
+    console.log(`${z}/${x}/${y}(${q}) ${JSON.stringify(r)} ${JSON.stringify(r2)}`)
+  }, () => {
+    db.close()
+  })
+}
